c864941bc53a5b4c9c81b8f5544dc4df9f401ff2
AdamHoodBB8 <a_hood_1@hotmail.com>
2021-02-02 20:37:00 +0000

    WIP 2.5D Concept

---
+++ Assets/BugNet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugNet : MonoBehaviour, IInventoryItem
+{
+
+    public string Name
+    {
+        get
+        {
+            return "BugNet";
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+    public void OnPickup()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
