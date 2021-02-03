1e8b332382d42193f4cf3c362aa855fe420da0ad
AdamHoodBB8 <a_hood_1@hotmail.com>
2021-02-03 14:14:17 +0000

    Merge of 3D and 2D Unity Projects

---
+++ Mess About/Assets/Hoop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoop : MonoBehaviour, IInventoryItem
+{
+
+    public string Name
+    {
+        get
+        {
+            return "Hoop";
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+    public void OnPickup()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Mess About/Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator m_Animator;
+    public bool moving;
+    public Inventory inventory;
+
+    private void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        moving = false;
+    }
+
+    private void Update()
+    {
+        if (moving == false)
+            m_Animator.SetBool("moving", false);
+
+        if (moving == true)
+            m_Animator.SetBool("moving", true);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            moving = true;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                agent.SetDestination(hit.point);
+                }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            moving = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IInventoryItem item = other.GetComponent<IInventoryItem>();
+        if (item != null)
+        {
+            inventory.AddItem(item);
+        }
+    }
+}
