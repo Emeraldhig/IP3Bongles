1110de01fdfe10f0ebd005c3b39889e0785f2a62
AdamHoodBB8 <a_hood_1@hotmail.com>
2021-02-09 22:48:57 +0000

    Key Door Open WIP
    Key minigame WIP added, still needs refined and animation added

---
+++ Mess About/Assets/KeyRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyRotate : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    public GameObject camera1;
+    public GameObject camera2;
+    public GameObject Gate;
+    public Mesh OpenGate;
+    public GameObject RotateKeyMaster;
+
+    private void Update()
+    {
+
+        Debug.Log(transform.eulerAngles.z);
+        if (transform.eulerAngles.z >= 300)
+        {
+            Gate.GetComponent<MeshFilter>().sharedMesh = OpenGate;
+            camera1.SetActive(false);
+            RotateKeyMaster.SetActive(false);
+            camera2.SetActive(true);
+        }
+    }
+
+
+    private void OnMouseDrag()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed);
+    }
+}
---
+++ Mess About/Assets/KeyTurn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyTurn : MonoBehaviour
+{
+
+    public Camera camera1;
+    public GameObject camera1Obj;
+    public GameObject camera2;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    public float moveSpeed = 0.1f;
+    public GameObject Key1;
+    public GameObject Key2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnMouseDrag()
+    {
+        float distance_to_screen = camera1.WorldToScreenPoint(gameObject.transform.position).z;
+        transform.position = camera1.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("hello");
+        camera2.SetActive(true);
+        Key2.SetActive(true);
+        camera1Obj.SetActive(false);
+        Key1.SetActive(false);
+    }
+
+}
