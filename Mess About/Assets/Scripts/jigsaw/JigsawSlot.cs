9b5f6db7acb517b4de9dd8e50290cf0d06cf6bf6
Emeraldhig <72360625+Emeraldhig@users.noreply.github.com>
2021-02-16 20:57:57 +0000

    jigsaw implementation

---
+++ Mess About/Assets/Scripts/jigsaw/JigsawSlot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JigsawSlot : MonoBehaviour
+{
+    public GameObject slot;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == slot.tag)
+        {
+            other.transform.position = slot.transform.position;
+            Debug.Log("slot:" + slot.tag + " filled");
+            other.GetComponent<pieceMovement>().locked = true;
+        }
+    }
+}
