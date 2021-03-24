40db7f9a48392b2dbe1c57ef4ccfb6622e4a5dd9
RWilsonUni <RWILSO241@caledonian.ac.uk>
2021-03-24 21:46:10 +0000

    Saving position between scenes
    Script for exiting scenes (needs applied)

---
+++ Mess About/Assets/Scripts/Scene Management/PositionSaveGuide.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class PositionSaveGuide // Static class to remember bubbas position through out each scene.
+{
+    public static Dictionary<int, Vector3> positionSaver = new Dictionary<int, Vector3>();
+}
